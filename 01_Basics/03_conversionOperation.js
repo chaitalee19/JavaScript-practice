36718ba384d2befb00d38b2f5a760a4973e2b411
chaitalee <132096020+chaitalee19@users.noreply.github.com>
2025-01-05 06:30:09 +0000

    #Basics JavaScript

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,114 @@
+//let score = "33"
+//console.log(typeof score); //one way of using
+//console.log(typeof (score)); //second way of using
+
+
+//-----Case : NaN-----
+//let score1="333vivek"
+//Coverting it into number 
+//console.log(typeof(score1))
+
+//let valueIsNumber = Number(score1)
+//console.log(typeof(valueIsNumber)) 
+//Data type is showing a number but when we put the value then we are getting NaN 9(Not a Number))  
+
+//----Case : null-----
+
+//let v1 = null
+
+// console.log(typeof(v1))//Object
+// let checkIsNumber = Number(v1)
+// console.log(typeof (checkIsNumber))//Number
+// console.log(checkIsNumber)//0
+
+//-----Case : Undefined ----- 
+// let v2 = undefined
+
+// console.log(typeof(v2))//Undefine
+// let checkIsNumber2 = Number(v2)
+// console.log(typeof (checkIsNumber2))//Number
+// console.log(checkIsNumber2)//NaN
+
+
+//-----Case: boolean-----
+
+// let checkval = true
+
+// console.log(typeof(checkval))//boolean
+// let checkIsNumber3 = Number(v1)
+// console.log(typeof (checkIsNumber3))//Number
+// console.log(checkIsNumber3)//True 1 false 0
+
+
+//-----Case : String-----
+
+// let stringval = "Chaitalee Madam"
+
+// console.log(typeof(stringval))//string
+// let checkIsNumber4 = Number(stringval)
+// console.log(typeof (checkIsNumber3))//Number
+// console.log(checkIsNumber3)//NaN
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+//let isLoggedIn=1
+//let isLoggedIn = ""   //false
+
+// let isLoggedIn = "Vivek" //True
+
+// let booleanIsLogged = Boolean(isLoggedIn) //boolean
+// console.log(booleanIsLogged)//True
+
+
+//1=>true
+//""=>false
+//"Vivek"=>true
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) //33
+// console.log(typeof(stringNumber))//String 
+
+
+
+//************************Operations**************//
+let value = 3 
+
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2) // Addition
+// console.log(2-2) // Substraction
+// console.log(2*2) // Multiplication
+// console.log(2/2) // Divide
+// console.log(2%2) //Reminder
+// console.log(2**2) //power of 2
+// console.log(2**3) //power of 3
+
+//Concatination
+// let name1 = "Vivek "
+// let name2 = "& Chaitu"
+
+// console.log(name1+name2)
+
+// console.log("1"+2);//12 
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+//Rule: String jar pahila asel tr tyach concatination hot 
+//Rule: Jar string last la asel ani pahila kahi operation astil tr pahila operation perform hotat ani mg string concatination hote 
+
+// console.log(true)//True
+// console.log(+true)//1
+// console.log(true+);//Expression expected.
+// console.log(+"");// Value 0 yeil
+
+// let num1,num2,num3;
+// num1 = num2 =num3=2+2;
+//Code readabilty should be better not follow this practice
+
+   
